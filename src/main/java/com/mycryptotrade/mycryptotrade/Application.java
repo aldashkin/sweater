6344e135aae82aa39439f3bc5dbfae0481e593f5
aldashkin <aldashkin@bk.ru>
2020-02-17 01:02:35 +0300

    ++

--- src/main/java/com/mycryptotrade/mycryptotrade/Application.java
+++ src/main/java/com/mycryptotrade/mycryptotrade/Application.java
@@ -1,5 +1,6 @@
 package com.mycryptotrade.mycryptotrade;
 
+
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DateUtil;
@@ -9,6 +10,9 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -78,52 +82,13 @@ public class Application {
 			String PoloniexXRP = price[18];
 			String HitbtcXRP = price[19];
 
+
+
 			try {
 				Thread.sleep(60000);
 			} catch (InterruptedException e) {
 
 			}
-
-//		for (String st : price) {
-//			System.out.println(st);
-//		}
-
-
-//		System.out.println(text);
-
-//		FileInputStream fis = new FileInputStream("C:/Users/work/Desktop/spring-boot-app/spring-boot-app/primer.xls");
-//		Workbook wb = new HSSFWorkbook(fis);
-//		String result = wb.getSheetAt(0).getRow(0).getCell(0).getStringCellValue();
-//		System.out.println(result);
-//
-//		System.out.println(getCellText(wb.getSheetAt(0).getRow(0).getCell(1)));
-//
-//		fis.close();
-//
-//	}
-//
-//	public static String getCellText(Cell cell){
-//
-//		String result="";
-//
-//		switch (cell.getCellType()) {
-//			case Cell.CELL_TYPE_STRING:
-//				result = cell.getRichStringCellValue().getString();
-//				break;
-//			case Cell.CELL_TYPE_NUMERIC:
-//				if (DateUtil.isCellDateFormatted(cell)) {
-//					result = cell.getDateCellValue().toString();
-//				} else {
-//					result = Double.toString(cell.getNumericCellValue());
-//				}
-//				break;
-//			case Cell.CELL_TYPE_BOOLEAN:
-//				result = Boolean.toString(cell.getBooleanCellValue());
-//				break;
-//			default:
-//				break;
-//		}
-//		return result;
 		}
 	}
 }
--- src/main/java/com/mycryptotrade/mycryptotrade/GreetingController.java
+++ src/main/java/com/mycryptotrade/mycryptotrade/GreetingController.java
@@ -5,23 +5,67 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
     @GetMapping("/index")
-    public String index(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model
-    ) {
-        model.put("name", name);
+    public String index(Model model) {
+
+        List<String> BinanceBTC;
+        List<String> PoloniexBTC;
+        List<String> HitbtcBTC;
+        List<String> BinanceETH;
+        List<String> PoloniexETH;
+        List<String> HitbtcETH;
+        List<String> BinanceZEC;
+        List<String> PoloniexZEC;
+        List<String> HitbtcZEC;
+        List<String> BinanceATOM;
+        List<String> PoloniexATOM;
+        List<String> HitbtcATOM;
+        List<String> BinanceXRP;
+        List<String> PoloniexXRP;
+        List<String> HitbtcXRP;
+
+        String b = "qwer";
+
+        BinanceBTC = Arrays.asList(b);
+        PoloniexBTC = Arrays.asList(b);
+        HitbtcBTC = Arrays.asList(b);
+        BinanceETH = Arrays.asList(b);;
+        PoloniexETH = Arrays.asList(b);
+        HitbtcETH = Arrays.asList(b);
+        BinanceZEC = Arrays.asList(b);
+        PoloniexZEC = Arrays.asList(b);
+        HitbtcZEC = Arrays.asList(b);
+        BinanceATOM = Arrays.asList(b);
+        PoloniexATOM = Arrays.asList(b);
+        HitbtcATOM = Arrays.asList(b);
+        BinanceXRP = Arrays.asList(b);
+        PoloniexXRP = Arrays.asList(b);
+        HitbtcXRP = Arrays.asList(b);
+
+
+        model.addAttribute("BinanceBTC", BinanceBTC);
+        model.addAttribute("PoloniexBTC", PoloniexBTC);
+        model.addAttribute("HitbtcBTC", HitbtcBTC);
+        model.addAttribute("BinanceETH", BinanceETH);
+        model.addAttribute("PoloniexETH", PoloniexETH);
+        model.addAttribute("HitbtcETH", HitbtcETH);
+        model.addAttribute("BinanceZEC", BinanceZEC);
+        model.addAttribute("PoloniexZEC", PoloniexZEC);
+        model.addAttribute("HitbtcZEC", HitbtcZEC);
+        model.addAttribute("BinanceATOM", BinanceATOM);
+        model.addAttribute("PoloniexATOM", PoloniexATOM);
+        model.addAttribute("HitbtcATOM", HitbtcATOM);
+        model.addAttribute("BinanceXRP", BinanceXRP);
+        model.addAttribute("PoloniexXRP", PoloniexXRP);
+        model.addAttribute("HitbtcXRP", HitbtcXRP);
+
+
         return "index";
-    }
-
-    @GetMapping
-    public String main(Map<String, String> model) {
-
-        model.put("some", "help!");
-        return "main";
     }
 }
