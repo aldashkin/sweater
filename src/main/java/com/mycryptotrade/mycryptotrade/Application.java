121be5de240a491ad8f5209b0883ae74878233db
aldashkin <aldashkin@bk.ru>
2020-02-16 16:02:10 +0300

    ++

--- src/main/java/com/mycryptotrade/mycryptotrade/Application.java
+++ src/main/java/com/mycryptotrade/mycryptotrade/Application.java
@@ -1,12 +1,40 @@
 package com.mycryptotrade.mycryptotrade;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args){
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws IOException {
 
+        SpringApplication.run(Application.class, args);
+        File excelFile = new File("BTC.xlsx");
+        FileInputStream fis = new FileInputStream(excelFile);
+
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        Iterator<Row> rowIt = sheet.iterator();
+
+        while (rowIt.hasNext()) {
+            Row row = rowIt.next();
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                System.out.println(cell.toString() + ";");
+            }
+            System.out.println();
+        }
+        workbook.close();
+        fis.close();
     }
 }
